f2f1a9ca2a13ba835b1b8bdcf7ca2ff08f67a97c
Satish <satish7275@gmail.com>
2026-08-27 11:55:52 -0400

    fix git configuration email

--- app.js
+++ app.js
@@ -9,18 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(cors());
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
 console.log("MongoDB Connected");
 })
 .catch(err => console.log(err));
+
  app.use('/api/users', require("./routes/userRoutes"));
-app.get('/',(req, res) => {
-   res.render('register');
-});
  const PORT = process.env.PORT || 5000;
  app.listen(PORT , ()=>{
     console.log('server running on port ${PORT}');
